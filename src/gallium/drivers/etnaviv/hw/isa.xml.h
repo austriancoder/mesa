547fc3da18dd55275a59e582c19d3e52081585cb
Christian Gmeiner <christian.gmeiner@gmail.com>
2017-04-22 16:02:06 +0200

    etnaviv: update generated headers
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/drivers/etnaviv/hw/isa.xml.h
+++ src/gallium/drivers/etnaviv/hw/isa.xml.h
@@ -8,8 +8,8 @@ http://0x04.net/cgit/index.cgi/rules-ng-ng
 git clone git://0x04.net/rules-ng-ng
 
 The rules-ng-ng source files this header was generated from are:
-- isa.xml       (  34392 bytes, from 2017-04-13 12:44:36)
-- copyright.xml (   1597 bytes, from 2016-10-29 07:29:22)
+- isa.xml       (  34392 bytes, from 2017-04-20 21:24:02)
+- copyright.xml (   1597 bytes, from 2017-04-20 21:09:20)
 
 Copyright (C) 2012-2017 by the following authors:
 - Wladimir J. van der Laan <laanwj@gmail.com>
