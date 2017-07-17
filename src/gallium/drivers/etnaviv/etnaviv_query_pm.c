da898e7dc26baea0bbad8e9fe3d95bc2213e9e7e
Christian Gmeiner <christian.gmeiner@gmail.com>
2017-07-17 21:25:04 +0200

    etnaviv: support TX performance counters
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/drivers/etnaviv/etnaviv_query_pm.c
+++ src/gallium/drivers/etnaviv/etnaviv_query_pm.c
@@ -320,6 +320,78 @@ static const struct etna_perfmon_config query_config[] = {
       .source = (const struct etna_perfmon_source[]) {
          { 1, "RA", "CULLED_QUAD_COUNT" }
       }
+   },
+   {
+      .name = "tx-total-bilinear-requests",
+      .type = ETNA_QUERY_TX_TOTAL_BILINEAR_REQUESTS,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "TOTAL_BILINEAR_REQUESTS" }
+      }
+   },
+   {
+      .name = "tx-total-trilinear-requests",
+      .type = ETNA_QUERY_TX_TOTAL_TRILINEAR_REQUESTS,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "TOTAL_TRILINEAR_REQUESTS" }
+      }
+   },
+   {
+      .name = "tx-total-discarded-texutre-requests",
+      .type = ETNA_QUERY_TX_TOTAL_DISCARDED_TEXTURE_REQUESTS,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "TOTAL_DISCARDED_TEXTURE_REQUESTS" }
+      }
+   },
+   {
+      .name = "tx-total-texutre-requests",
+      .type = ETNA_QUERY_TX_TOTAL_TEXTURE_REQUESTS,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "TOTAL_TEXTURE_REQUESTS" }
+      }
+   },
+   {
+      .name = "tx-mem-read-count",
+      .type = ETNA_QUERY_TX_MEM_READ_COUNT,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "MEM_READ_COUNT" }
+      }
+   },
+   {
+      .name = "tx-mem-read-in-8b-count",
+      .type = ETNA_QUERY_TX_MEM_READ_IN_8B_COUNT,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "MEM_READ_IN_8B_COUNT" }
+      }
+   },
+   {
+      .name = "tx-cache-miss-count",
+      .type = ETNA_QUERY_TX_CACHE_MISS_COUNT,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "CACHE_MISS_COUNT" }
+      }
+   },
+   {
+      .name = "tx-cache-hit-texel-count",
+      .type = ETNA_QUERY_TX_CACHE_HIT_TEXEL_COUNT,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "CACHE_HIT_TEXEL_COUNT" }
+      }
+   },
+   {
+      .name = "tx-cache-miss-texel-count",
+      .type = ETNA_QUERY_TX_CACHE_MISS_TEXEL_COUNT,
+      .nr_sources = 1,
+      .source = (const struct etna_perfmon_source[]) {
+         { 1, "TX", "CACHE_MISS_TEXEL_COUNT" }
+      }
    }
 };
 
